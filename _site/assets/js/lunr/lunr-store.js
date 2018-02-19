1c5e370c62b749d01c98c9c0af325c3567f9359b
Jin Zhe <spyrant@gmail.com>
2018-02-19 18:13:19 +0800

    first post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Installing Caffe2 with CUDA on Anaconda 2",
+        "excerpt":"It is often the case in research labs that you do not get access to admin priviledges and so installation of AI tools can be nerve-racking.The following guide shows you how to install install Caffe2 with CUDA under the Anaconda 2 virtual environment without needing to use sudo. This guide...","categories": ["caffe2","CUDA","cuDNN"],
+        "tags": [],
+        "url": "http://localhost:4000/caffe2/cuda/cudnn/installing-caffe2-with-cuda-on-anaconda2/",
+        "teaser":null}]
