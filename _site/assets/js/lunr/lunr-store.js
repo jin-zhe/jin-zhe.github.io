229cc31c45c5bae1c29187ed4d3511e3587805a4
Jin Zhe <spyrant@gmail.com>
2019-03-07 15:18:36 +0800

    fix conda instructions

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,21 +1,26 @@
 var store = [{
-        "title": "Getting cuDNN",
-        "excerpt":"This may be a too short to justify a stand-alone post but since many of myguides requires this bit of instruction, I felt it was necessary to abstract itout separately.In this guide I will show you how to get the right version of cuDNN and where toplace it. Unless stated...","categories": ["Guides"],
+        "title": "Getting CUDA with cuDNN",
+        "excerpt":"In this guide I will show you how to install CUDA with cuDNN in your home directory without invoking admin privileges. Unless stated otherwise, we will be using this configuration for all of my guides. Assumptions Ubuntu OS 16.04 NVIDIA GPU with driver installed What is From NVIDIA: CUDA® is...","categories": ["Guides"],
         "tags": ["cuDNN","CUDA","NVIDIA"],
         "url": "http://localhost:4000/guides/getting-cudnn/",
         "teaser":null},{
         "title": "Getting up to speed with Anaconda",
-        "excerpt":"Anaconda is a freemium open-source software for Python and R and itis widely used in scientific computing. It provides a virtual environmentmanager and it also comes with its own package management ecosystem conda. Whydoes this matter you ask? Be it in academic or industrial research labs, it isoften the case...","categories": ["Guides"],
+        "excerpt":"Anaconda is a freemium open-source software for Python and R and it is widely used in scientific computing. It provides a virtual environment manager and it also comes with its own package management ecosystem conda. Why does this matter you ask? Be it in academic or industrial research labs, it...","categories": ["Guides"],
         "tags": ["anaconda","conda"],
         "url": "http://localhost:4000/guides/getting-up-to-speed-with-anaconda/",
         "teaser":null},{
         "title": "Installing Caffe2 with CUDA on Anaconda",
-        "excerpt":"The following guide shows you how to install install Caffe2 with CUDAunder the Anaconda 2 virtual environment without needing to use sudo. Thisguide is meant for machines running on Ubuntu 16.04 equipped with NVIDIA GPUswith CUDA support. i.e it assumes CUDA is already installed by a system admin.As caffe2 is...","categories": ["Guides"],
+        "excerpt":"The following guide shows you how to install install Caffe2 with CUDA under the Anaconda 2 virtual environment without needing to use sudo. This guide is meant for machines running on Ubuntu 16.04 equipped with NVIDIA GPUs with CUDA support. i.e it assumes CUDA is already installed by a system...","categories": ["Guides"],
         "tags": ["caffe2","CUDA","cuDNN","anaconda"],
         "url": "http://localhost:4000/guides/installing-caffe2-with-cuda-on-anaconda/",
         "teaser":null},{
         "title": "Installing Caffe with CUDA on Anaconda",
-        "excerpt":"The following guide shows you how to install install Caffe with CUDAunder the Anaconda 2 virtual environment.Assumptions Ubuntu OS NVIDIA GPU with CUDA support Anaconda 2 (see installation instructions here) CUDA (installed by system admin)SpecificationsThis guide is written for the following specs: Ubuntu 16.04 Python 2.7 CUDA 8 cuDNN v7.1...","categories": ["Guides"],
+        "excerpt":"The following guide shows you how to install install Caffe with CUDA under the Anaconda 2 virtual environment. Assumptions Ubuntu OS NVIDIA GPU with CUDA support Anaconda 2 (see installation instructions here) CUDA (installed by system admin) Specifications This guide is written for the following specs: Ubuntu 16.04 Python 2.7...","categories": ["Guides"],
         "tags": ["caffe","CUDA","cuDNN","anaconda"],
         "url": "http://localhost:4000/guides/installing-caffe-with-cuda-on-anaconda/",
+        "teaser":null},{
+        "title": "Installing pytorch with CUDA on Anaconda",
+        "excerpt":"The following guide shows you how to install pytorch with CUDA under the Anaconda 2 virtual environment. Assumptions Ubuntu OS NVIDIA GPU with CUDA support Anaconda 2 (see installation instructions here) CUDA (installed by system admin) Specifications This guide is written for the following specs: Ubuntu 16.04 Python 2.7 CUDA...","categories": ["Guides"],
+        "tags": ["pytorch","CUDA","cuDNN","anaconda"],
+        "url": "http://localhost:4000/guides/installing-pytorch-with-cuda-on-anaconda/",
         "teaser":null}]
